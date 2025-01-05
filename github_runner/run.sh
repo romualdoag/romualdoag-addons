0efe3ec731ac8401066b1077b3d976b2c05eb2ce
Romualdo Alcantara <5167140+romualdoag@users.noreply.github.com>
2025-01-05 00:50:44 -0300

    chore: remove crazy sudo

--- github_runner/run.sh
+++ github_runner/run.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/with-contenv bashio
 
-if ! test -w /var/run/docker.sock
-then
-  # This process does not have write access to the Docker socket. This needs to
-  # be fixed.
-  #
-  # Look up which group (by id) owns the Docker socket
-  docker_gid="$(stat -c '%g' /var/run/docker.sock)"
+# if ! test -w /var/run/docker.sock
+# then
+#   # This process does not have write access to the Docker socket. This needs to
+#   # be fixed.
+#   #
+#   # Look up which group (by id) owns the Docker socket
+#   docker_gid="$(stat -c '%g' /var/run/docker.sock)"
 
-  # Now create a group in this container with that gid, called 'docker'
-  sudo groupadd --gid "$docker_gid" docker
+#   # Now create a group in this container with that gid, called 'docker'
+#   sudo groupadd --gid "$docker_gid" docker
 
-  # Add the runner user (which executes this script) to that group
-  sudo adduser runner docker
+#   # Add the runner user (which executes this script) to that group
+#   sudo adduser runner docker
 
-  # Adding the user to the group is not effective for the bash process that is
-  # executing this script. Lets call this script again through sudo. We set the
-  # user to 'runner' again, but sudo looks up which groups this user is in and
-  # equips the new process accordingly.
-  exec sudo --preserve-env --user=runner "$0" "$@"
-fi
+#   # Adding the user to the group is not effective for the bash process that is
+#   # executing this script. Lets call this script again through sudo. We set the
+#   # user to 'runner' again, but sudo looks up which groups this user is in and
+#   # equips the new process accordingly.
+#   exec sudo --preserve-env --user=runner "$0" "$@"
+# fi
 
 cd
 
